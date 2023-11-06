76bc3923babbc265315c851769b087524737a603
sd-david <david@sauberdigital.de>
2023-11-06 11:58:54 +0100

    initial

---
+++ sd_symfony_secret_create.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+vendor="sd"
+default_prefix="project"
+
+# Ask for project prefix and save in var. Use default if no input is given.
+read -p "Project Prefix (default: ${default_prefix}): " project_prefix
+project_prefix=${project_prefix:-$default_prefix}
+
+# Generate random string and save in var.
+random_string=$(openssl rand -hex 16)
+
+# Check if openssl command succeeded.
+if [ $? -ne 0 ]; then
+    echo "Error generating random string. Is openssl installed?"
+    exit 1
+fi
+
+# Combine all.
+secret="${vendor}${project_prefix}${random_string}"
+
+# Final result output.
+echo "Generated APP_SECRET: $secret"
