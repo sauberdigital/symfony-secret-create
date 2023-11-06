acb46fabc2137a9392860e1e2d923c8c5dd57a99
sd-david <david@sauberdigital.de>
2023-11-06 12:03:47 +0100

    limited project prefix

--- sd_symfony_secret_create.sh
+++ sd_symfony_secret_create.sh
@@ -1,11 +1,20 @@
+read -p "Project Prefix [${default_prefix}]: " project_prefix
+
 #!/bin/bash
 
 vendor="sd"
 default_prefix="project"
+valid_input_regex='^[0-9a-zA-Z]{1,25}$'
 
-# Ask for project prefix and save in var. Use default if no input is given.
+# Ask for project prefix and use default if no input is given.
 read -p "Project Prefix [${default_prefix}]: " project_prefix
 project_prefix=${project_prefix:-$default_prefix}
+
+# Validate project prefix.
+if ! [[ $project_prefix =~ $valid_input_regex ]]; then
+    echo "Invalid input. Only letters and numbers are allowed, up to 25 characters."
+    exit 1
+fi
 
 # Generate random string and save in var.
 random_string=$(openssl rand -hex 16)
